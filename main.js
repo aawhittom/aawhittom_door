a19725450f4d8ddfcda5cc9f24aa94351df7e1b2
aawhittom <31892255+aawhittom@users.noreply.github.com>
2025-12-18 15:07:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ function init() {
 
     // Camera
     camera = new THREE.PerspectiveCamera(80, sizes.width / sizes.height, 0.1, 1000);
-    camera.position.set(0, -0.7,2.1);
+    camera.position.set(0, -0.7,1.1);
 
     // Controls
     controls = new OrbitControls(camera, renderer.domElement);
@@ -153,3 +153,4 @@ function animate() {
 
     renderer.render(scene, camera);
 }
+
